35f681c60aa894b1d861d1c7d47f9ef1c8de227d
Abdullah <abdallahmosbah25@gmail.com>
2023-06-16 15:04:45 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point to program.
+ * Return: 0
+ */
+int main(void)
+{
+    int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
