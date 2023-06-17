29f5343047722518e0c176d618446bb890c88565
Abdullah <abdallahmosbah25@gmail.com>
2023-06-17 10:24:02 -0700

    commit 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * main - entry to the main program
+ * Return: 0 (Success)
+ */
+
+int main()
+{
+	int i = 0;
+
+	while (i < 100) {
+	int first_digit = i / 10;
+	int second_digit = i % 10;
+
+	if (first_digit != second_digit && first_digit < second_digit) {
+		putchar('0' + first_digit);
+		putchar('0' + second_digit);
+		putchar(',');
+		putchar(' ');
+		}
+
+	i++;
+    }
+
+	putchar('\n');
+
+	return 0;
+}
+
