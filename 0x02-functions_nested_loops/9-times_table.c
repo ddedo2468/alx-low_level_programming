683366c788fe2f33a6c32a50b702694ddd9a8d92
Abdullah <abdallahmosbah25@gmail.com>
2023-06-20 16:31:38 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * tines_table - prints the time table to 9
+ * Description: printing all times table from 0 to 9
+ */
+void times_tables(void)
+{
+	int k, l, m;
+
+	for (k = 0; k <= 9; k++)
+	{
+		for (l = 0; l <= 9; l++)
+		{
+			m = k*l;
+
+			if (l == 0)
+			{
+				_putchar('0);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (m < 10)
+			{
+				_putchar(' ');
+				_putchar(m + '0;);
+			}
+			else
+			{
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
