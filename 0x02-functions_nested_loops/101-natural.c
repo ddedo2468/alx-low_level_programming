ceded37b1cf66d0b1790bb137391d4b29de5ce3c
Abdullah <abdallahmosbah25@gmail.com>
2023-06-21 13:27:43 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * sum_of_multiples - computes and prints 
+ * the sum of all the multiples 
+ * of 3 or 5 below 1024
+ * Return: the total of sum
+ */
+int sum_of_multiples()
+{
+	int total = 0;
+
+	for (int num = 1; num < 1024; num++)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			total += num;
+		}
+	}
+	return total;
+}
