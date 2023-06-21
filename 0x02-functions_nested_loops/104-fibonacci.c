1d80e863e6d3df1258bf5d89a2449f2281dfef06
Abdullah <abdallahmosbah25@gmail.com>
2023-06-21 14:25:13 -0700

    fixed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,35 +1,38 @@
 #include <stdio.h>
 #define LARGEST 10000000000
+/**
+ * main - main block
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ * Numbers should be coma and space separated.
+ * Return: 0
+ */
 int main(void)
 {
-	unsigned long int previous_1 = 0, previous_2 = 1, current_1 = 0, current_2 = 2;
-	unsigned long int carry_1, carry_2, carry_3;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
 	int count;
 
-	printf("%lu, %lu, ", previous_2, current_2);
-
+	printf("%lu, %lu, ", bk1, bk2);
 	for (count = 2; count < 98; count++)
 	{
-		if (current_1 + current_2 > LARGEST || previous_2 > 0 || previous_1 > 0)
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
 		{
-			carry_1 = (current_1 + current_2) / LARGEST;
-			carry_2 = (current_1 + current_2) % LARGEST;
-			carry_3 = previous_1 + previous_2 + carry_1;
-			previous_1 = previous_2, previous_2 = carry_3;
-			current_1 = current_2, current_2 = carry_2;
-			printf("%lu%010lu", previous_2, current_2);
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
 		}
 		else
 		{
-			carry_2 = previous_1 + previous_2;
-			previous_1 = previous_2, previous_2 = carry_2;
-			printf("%lu", previous_2);
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
 		}
-
 		if (count != 97)
 			printf(", ");
 	}
-
 	printf("\n");
 	return (0);
 }
