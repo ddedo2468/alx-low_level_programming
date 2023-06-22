7aac54453c655139e3d77f98ff76b777ce1a3a0e
Abdullah <abdallahmosbah25@gmail.com>
2023-06-22 13:36:41 -0700

    for instead of while

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,24 +4,18 @@
  * more_numbers - prina all nums 0 to 14 *10
  * followe by new line
  */
-int more_numbers(void)
+void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (j = 0; j < 10; j++)
+	for (i = 0; i < 10; i++)
 	{
-		i = 0;
-		while (i <= 14)
+		for (j = 0; j < 15; j++)
 		{
-			if (i >= 10)
-			{
-				_putchar(i / 10 + '0');
-			}
-				_putchar(i % 10 + '0');
-			i++;
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
-
 }
