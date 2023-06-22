5cd2b75dcb040b2e751d7ac2004e56d743c89092
Abdullah <abdallahmosbah25@gmail.com>
2023-06-22 13:19:32 -0700

    nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * more_numbers - prina all nums 0 to 14 *10
+ */
+int more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if (i >= 10)
+			{
+				putchar(i / 10 + '0');
+			}
+				putchar(i % 10 + '0');
+			i++;
+		}
+	putchar('\n');
+	}
+
+}
+
