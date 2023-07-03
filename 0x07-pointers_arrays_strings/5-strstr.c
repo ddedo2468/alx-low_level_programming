68dec9e815b492c3b4e09aae431dec04423a32f7
Abdullah <abdallahmosbah25@gmail.com>
2023-07-03 06:03:33 -0700

    fixed

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,18 +14,13 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (needle[0] == haystack[i])
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			for (j = 0; needle[j] != '\0'; j++)
-			{
-				if (needle[j] != haystack[i + j])
-							break;
-			}
-
-			if (needle[j] == '\0')
-			return (&haystack[i]);
+			if (haystack[i + j] != needle[j])
+				break;
 		}
+		if (!needle[j])
+			return (&haystack[i]);
 	}
-
 	return (NULL);
 }
