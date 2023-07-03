631ceec78a6abbf68b386f20806d782679280e6e
Abdullah <abdallahmosbah25@gmail.com>
2023-07-03 05:54:04 -0700

    fixed 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * _strstr - return pointer to @needle location in @haystack
  * @haystack: the place where we will search for @needle
@@ -11,14 +12,19 @@ char *_strstr(char *haystack, char *needle)
 	int i, j;
 
 	for (i = 0; haystack[i] != '\0'; i++)
+	{
 		if (needle[0] == haystack[i])
 		{
 			for (j = 0; needle[j] != '\0'; j++)
+			{
 				if (needle[j] != haystack[i + j])
-					break;
+							break;
+			}
 
 			if (needle[j] == '\0')
-				return (&haystack[i]);
+			return &haystack[i];
 		}
-	return ('\0');
+	}
+
+	return (NULL);
 }
