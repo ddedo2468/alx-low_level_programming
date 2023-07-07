8093c8027f9a4d87dfafb9b5ec15554cac7a0b87
Abdullah <abdallahmosbah25@gmail.com>
2023-07-07 05:27:22 -0700

    argcv+

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - the main code block
+ * main - print the name of the file followed by a new line
+ * @argc: argument number
+ * @argv: argument array of strings
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	
+	return (0);
 }
