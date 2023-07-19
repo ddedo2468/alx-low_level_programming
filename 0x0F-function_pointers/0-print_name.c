429f454bd93dae9a2e3c1ca28f25805e5c9caeb3
Abdullah <abdallahmosbah25@gmail.com>
2023-07-19 09:09:58 -0700

    code Mode

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - Print a name using function pointer
+ * @name: char str
+ * @f: function pointer with str arg
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
